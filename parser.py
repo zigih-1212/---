ee3e16b3805f2b83362954156bb3e4295a7cc4c0
zigih-1212 <dvybornyh332@gmail.com>
2026-06-15 02:39:37 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,1 +1,6 @@
-
+def is_video_processed(video_id: str) -> bool:
+    conn = get_db()
+    # Ищем в базе, был ли уже опубликован этот видео-ID
+    row = conn.execute("SELECT 1 FROM posts WHERE donor_post_id=?", (video_id,)).fetchone()
+    conn.close()
+    return row is not None
