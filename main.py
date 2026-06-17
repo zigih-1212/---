a2d67b550411e881efe9b9895a4b67927080f24b
zigih-1212 <dvybornyh332@gmail.com>
2026-06-17 11:44:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -34,6 +34,7 @@ from aiogram.filters import Command, CommandStart
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.exceptions import TelegramAPIError, TelegramBadRequest
 from aiogram.types import (
     CallbackQuery,
     InlineKeyboardButton,
