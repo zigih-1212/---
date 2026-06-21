cff8c59aae7f382a1af53ab5e1e93c7c389ddf56
zigih-1212 <dvybornyh332@gmail.com>
2026-06-21 14:11:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ import secrets
 import sqlite3
 import time
 import random
+import hashlib
 from xml.etree import ElementTree as ET
 from datetime import datetime, timedelta, timezone
 from logging.handlers import RotatingFileHandler
