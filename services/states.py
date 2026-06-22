9774559fdbec9e008d8cd260e5c48d2616ac253c
zigih-1212 <dvybornyh332@gmail.com>
2026-06-22 20:27:06 +0300

    Create states.py

---
+++ services/states.py
@@ -0,0 +1,31 @@
+from aiogram.fsm.state import State, StatesGroup
+
+class OnboardingStates(StatesGroup):
+    waiting_role = State()
+    waiting_channel = State()
+    waiting_source_channel = State()
+    waiting_saas_tg_channel = State()
+    waiting_target_choice = State()
+    waiting_video_link = State()
+
+class AdminStates(StatesGroup):
+    broadcast_text = State()
+    extend_user_id = State()
+    extend_days = State()
+
+class SaasStates(StatesGroup):
+    waiting_apikey = State()
+    waiting_erid_override = State()
+    waiting_promocode = State()
+    choosing_channel_for_promo = State()
+
+class PaymentFSM(StatesGroup):
+    choosing_tariff = State()
+    choosing_method = State()
+    waiting_for_receipt = State()
+    waiting_promocode = State()
+    choosing_channel_for_promo = State()
+
+class PayoutStates(StatesGroup):
+    waiting_for_card = State()
+    waiting_for_amount = State()
