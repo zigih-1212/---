3ee436d94de77afa8a86b5e2d26bc71a66c8a251
zigih-1212 <dvybornyh332@gmail.com>
2026-07-06 19:17:07 +0300

    Update text_rewriter.py

--- services/text_rewriter.py
+++ services/text_rewriter.py
@@ -19,14 +19,16 @@ def generate_post_text(title, price, currency, advertiser, erid, partner_url,
                        delivery_info="", promocode=""):
     template = random.choice(TEMPLATES)
     price_label = random.choice(PRICE_LABELS)
-    price_str = f"{int(price)}" if price == int(price) else f"{price:.2f}"
+
+    if price == int(price):
+        price_str = f"{int(price)}"
+    else:
+        price_str = f"{price:.2f}"
 
     discount_line = ""
-        if delivery_info:
-            short_delivery = delivery_info[:150].rstrip()
-            if len(delivery_info) > 150:
-                short_delivery += '...'
-            caption += f"\n🚚 {short_delivery}"
+    if old_price and discount_percent:
+        old_price_str = f"{int(old_price)}" if old_price == int(old_price) else f"{old_price:.2f}"
+        discount_line = f"\n🔥 Скидка {discount_percent}% (было {old_price_str} {currency})"
 
     link_text = f"<a href='{partner_url}'>Посмотреть и заказать</a>"
 
@@ -43,10 +45,17 @@ def generate_post_text(title, price, currency, advertiser, erid, partner_url,
         caption += f"\n🎟 Промокод: <code>{promocode}</code>"
 
     if delivery_info:
-        short_delivery = delivery_info[:200] + ('...' if len(delivery_info) > 200 else '')
+        short_delivery = delivery_info[:150].rstrip()
+        if len(delivery_info) > 150:
+            short_delivery += '...'
         caption += f"\n🚚 {short_delivery}"
 
     caption += f"\n\nРеклама. {advertiser}. Erid: {erid}"
     if adult:
         caption = "🔞 18+\n" + caption
+
+    # Защита от превышения длины подписи Telegram (1024 символа)
+    if len(caption) > 1000:
+        caption = caption[:1000] + '...'
+
     return caption
