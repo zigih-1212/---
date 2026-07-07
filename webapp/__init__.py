fe695df1f6303559161bcbcb5163b90d9c74def1
zigih-1212 <dvybornyh332@gmail.com>
2026-07-07 22:31:33 +0300

    Update __init__.py

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,14 +1,10 @@
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
 from webapp.routes_admin import router as admin_router
 from webapp.routes_user import router as user_router
-from webapp.ensure_files import ensure_files  # <-- импорт
 
 def create_app(bot) -> FastAPI:
-    ensure_files()  # <-- создаём файлы, если их нет
     app = FastAPI()
     app.state.bot = bot
-    app.mount("/static", StaticFiles(directory="webapp/static"), name="static")
     app.include_router(admin_router, prefix="/admin", tags=["admin"])
     app.include_router(user_router, prefix="/my-stats", tags=["user"])
     return app
