c394bd708f85c324cfc83c76e245f3a51598a752
zigih-1212 <dvybornyh332@gmail.com>
2026-07-14 18:04:57 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ from services.db import get_db
 from config import ADMIN_IDS
 from config import MIN_PAYOUT
 from config import BOT_USERNAME
-
+from aiogram.enums import ParseMode
 logger = logging.getLogger("autopost_bot.referral")
 
 def log_admin_action(admin_id: int, action: str, details: str = ""):
