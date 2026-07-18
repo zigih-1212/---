42163a52f88128cc5a218d7a15ffa5f798c3117e
Zigi <dvybornyh332@gmail.com>
2026-07-18 15:17:59 +0300

    fix

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 from webapp.routes_admin import router as admin_router
 from webapp.routes_user import router as user_router
+from webapp.routes_postback import router as postback_router
 
 def create_app(bot) -> FastAPI:
     app = FastAPI()
     app.state.bot = bot
     app.include_router(admin_router, prefix="/admin", tags=["admin"])
     app.include_router(user_router, prefix="/my-stats", tags=["user"])
+    app.include_router(postback_router, prefix="/postback", tags=["postback"])
     return app
